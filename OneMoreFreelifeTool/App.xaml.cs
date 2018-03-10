7a5dc813cdca4be4d76970f172d2c378e0fce0ee
akina <svoooc@gmail.com>
2018-03-10 11:25:23 +0900

    First Commit

---
+++ OneMoreFreelifeTool/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Livet;
+using SandBeige.OneMoreFreelifeOnlineTool.ViewModels;
+using SandBeige.OneMoreFreelifeOnlineTool.Views;
+
+namespace SandBeige.OneMoreFreelifeOnlineTool {
+	/// <summary>
+	/// App.xaml の相互作用ロジック
+	/// </summary>
+	public partial class App : Application {
+		protected override void OnStartup(StartupEventArgs e) {
+			DispatcherHelper.UIDispatcher = this.Dispatcher;
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+			this.MainWindow = new MainWindow() {
+				DataContext = new MainWindowViewModel()
+			};
+			this.MainWindow.ShowDialog();
+		}
+
+		//集約エラーハンドラ
+		private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+			if (e.ExceptionObject is Exception ex) {
+				Console.WriteLine(ex.Message);
+				Console.WriteLine(ex.StackTrace);
+			} else {
+				Console.WriteLine(e.ToString());
+			}
+
+			//TODO:ロギング処理など
+			MessageBox.Show(
+				$"不明なエラーが発生しました。アプリケーションを終了します。{e.ToString()}",
+				"エラー",
+				MessageBoxButton.OK,
+				MessageBoxImage.Error);
+			Environment.Exit(1);
+		}
+	}
+}
---
+++ OneMoreFreelifeTool/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Livet;
+
+namespace SandBeige.OneMoreFreelifeOnlineTool.ViewModels {
+	class MainWindowViewModel:ViewModel {
+
+		public MainWindowViewModel() {
+		}
+	}
+}
